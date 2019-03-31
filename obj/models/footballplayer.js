fbe9735c245ec5dd41a32344b1027e3bce0b4130
tekh3999 <tekh4999@mail.com>
2019-03-31 18:24:33 -0500

    update
    updatemarkers

---
+++ obj/models/footballplayer.js
@@ -0,0 +1,73 @@
+{
+
+"metadata":
+{
+"sourceFile": "",
+"generatedBy": "3ds max ThreeJSExporter",
+"formatVersion": 3.1,
+"vertices": 108843,
+"normals": 213041,
+"colors": 0,
+"uvs": 114789,
+"triangles": 213041,
+"materials": 66
+},
+
+"materials": [
+{
+"DbgIndex" : 0,
+"DbgName"  : "4_InnerMouth",
+"colorDiffuse"  : [1.0000, 1.0000, 1.0000],
+"colorAmbient"  : [0.0000, 0.0000, 0.0000],
+"colorSpecular"  : [0.5922, 0.7294, 0.7922],
+"transparency"  : 1.0,
+"specularCoef"  : 0.844999,
+"mapDiffuse"    : "M4Smpl_mouth.jpg",
+"vertexColors" : false
+},
+
+{
+"DbgIndex" : 1,
+"DbgName"  : "4_Gums",
+"colorDiffuse"  : [1.0000, 1.0000, 1.0000],
+"colorAmbient"  : [0.9020, 0.8549, 0.7647],
+"colorSpecular"  : [0.5922, 0.7294, 0.7922],
+"transparency"  : 1.0,
+"specularCoef"  : 0.844999,
+"mapDiffuse"    : "M4Smpl_mouth.jpg",
+"vertexColors" : false
+},
+
+{
+"DbgIndex" : 2,
+"DbgName"  : "4_Teeth",
+"colorDiffuse"  : [1.0000, 1.0000, 1.0000],
+"colorAmbient"  : [0.9020, 0.8549, 0.7647],
+"colorSpecular"  : [0.5922, 0.7294, 0.7922],
+"transparency"  : 1.0,
+"specularCoef"  : 0.844999,
+"mapDiffuse"    : "M4Smpl_mouth.jpg",
+"vertexColors" : false
+},
+
+{
+"DbgIndex" : 3,
+"DbgName"  : "4_Tongue",
+"colorDiffuse"  : [1.0000, 1.0000, 1.0000],
+"colorAmbient"  : [0.0000, 0.0000, 0.0000],
+"colorSpecular"  : [0.5922, 0.7294, 0.7922],
+"transparency"  : 1.0,
+"specularCoef"  : 0.844999,
+"mapDiffuse"    : "M4Smpl_mouth.jpg",
+"vertexColors" : false
+},
+
+{
+"DbgIndex" : 4,
+"DbgName"  : "2_SkinHead",
+"colorDiffuse"  : [1.0000, 1.0000, 1.0000],
+"colorAmbient"  : [1.0000, 1.0000, 1.0000],
+"colorSpecular"  : [0.0784, 0.1020, 0.1098],
+"transparency"  : 1.0,
+"specularCoef"  : 0.75,
+"mapDiffuse"    : "M4Smpl_malebody.jpg",
